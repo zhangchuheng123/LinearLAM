5a6c00402cb9471b03c5cf3b5df2fae87b454833
Chuheng Zhang <zhangchuheng123@live.com>
2025-05-12 00:10:03 +0000

    update

--- scripts/6.py
+++ scripts/6.py
@@ -2,6 +2,7 @@ from sklearn.cross_decomposition import CCA
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -69,13 +70,22 @@ def main(learn_A, CD_zero, pseudo_latent):
     db = 128    # dimension of noise prediction
 
     dz_list = list(range(2, 17))
-    kappa_coeff_list = [0, 0.01, 0.1]       # magnitude of kappa
+    kappa_coeff_list = [0, 0.01, 0.1, 0.5]       # magnitude of kappa
     sigma_list = [0, 0.1, 0.5, 1.0]
     
+    # existing = pd.read_csv('5_learnATrue_CDzeroFalse_psdactionTrue_morechi_evalrand.csv', index_col=0)
+    # record = existing.to_dict('records')
     record = []
+    tic = time.time()
+
     for sigma, dz, kappa_coeff in product(sigma_list, dz_list, kappa_coeff_list):
 
-        print(f'noise chi={sigma} kappa_coeff={kappa_coeff} dz={dz}')
+        print(f'noise sigma={sigma} kappa_coeff={kappa_coeff} dz={dz} time={time.time() - tic:.2f}s')
+        tic = time.time()
+        # matching_record = [item for item in record if item['sigma'] == sigma and item['dz'] == dz and item['kappa_coeff'] == kappa_coeff and item['iter'] == 40000]
+
+        # if len(matching_record) > 0:
+        #     continue
 
         action_embeddings = np.random.randn(do, da)
         action_embeddings, _ = np.linalg.qr(action_embeddings) 
@@ -117,63 +127,62 @@ def main(learn_A, CD_zero, pseudo_latent):
             loss.backward()
             opt1.step()
 
-            if i_batch % 1000 == 0:
-                # Evaluation
-                for i_eval in range(5000):
+        # Evaluation
+        for i_eval in range(5000):
 
-                    O = np.random.randn(do, N)
-                    # A = (1 - chi) * np.random.randn(da, N) + chi * policy_embeddings @ O
-                    A = np.random.randn(da, N)
-                    Q = action_embeddings @ A 
-                    noise = np.random.rand(do, N) * sigma
+            O = np.random.randn(do, N)
+            # A = (1 - chi) * np.random.randn(da, N) + chi * policy_embeddings @ O
+            A = np.random.randn(da, N)
+            Q = action_embeddings @ A 
+            noise = np.random.rand(do, N) * sigma
 
-                    Op = O + Q + noise
+            Op = O + Q + noise
 
-                    tensor_O = torch.tensor(O.T, dtype=torch.float32)
-                    tensor_Op = torch.tensor(Op.T, dtype=torch.float32)
-                    target_A = torch.tensor(A.T, dtype=torch.float32)
-                    target_O = torch.tensor(O.T, dtype=torch.float32)
-                    target_N = torch.tensor(noise.T, dtype=torch.float32)
+            tensor_O = torch.tensor(O.T, dtype=torch.float32)
+            tensor_Op = torch.tensor(Op.T, dtype=torch.float32)
+            target_A = torch.tensor(A.T, dtype=torch.float32)
+            target_O = torch.tensor(O.T, dtype=torch.float32)
+            target_N = torch.tensor(noise.T, dtype=torch.float32)
 
-                    _, preds = lam(tensor_O, tensor_Op)
-                    act, obs, noi = preds
-                    loss = nn.MSELoss()(act, target_A) + nn.MSELoss()(obs, target_O) + nn.MSELoss()(noi, target_N)
-                    opt2.zero_grad()
-                    loss.backward()
-                    opt2.step()
+            _, preds = lam(tensor_O, tensor_Op)
+            act, obs, noi = preds
+            loss = nn.MSELoss()(act, target_A) + nn.MSELoss()(obs, target_O) + nn.MSELoss()(noi, target_N)
+            opt2.zero_grad()
+            loss.backward()
+            opt2.step()
 
-                if True:
+        if True:
 
-                    O = np.random.randn(do, NN)
-                    # A = (1 - chi) * np.random.randn(da, NN) + chi * policy_embeddings @ O
-                    A = np.random.randn(da, NN)
-                    Q = action_embeddings @ A 
-                    noise = np.random.rand(do, NN) * sigma
+            O = np.random.randn(do, NN)
+            # A = (1 - chi) * np.random.randn(da, NN) + chi * policy_embeddings @ O
+            A = np.random.randn(da, NN)
+            Q = action_embeddings @ A 
+            noise = np.random.rand(do, NN) * sigma
 
-                    Op = O + Q + noise
+            Op = O + Q + noise
 
-                    tensor_O = torch.tensor(O.T, dtype=torch.float32)
-                    tensor_Op = torch.tensor(Op.T, dtype=torch.float32)
-                    target_A = torch.tensor(A.T, dtype=torch.float32)
-                    target_O = torch.tensor(O.T, dtype=torch.float32)
-                    target_N = torch.tensor(noise.T, dtype=torch.float32)
-                    obsp, preds = lam(tensor_O, tensor_Op)
-                    act, obs, noi = preds
+            tensor_O = torch.tensor(O.T, dtype=torch.float32)
+            tensor_Op = torch.tensor(Op.T, dtype=torch.float32)
+            target_A = torch.tensor(A.T, dtype=torch.float32)
+            target_O = torch.tensor(O.T, dtype=torch.float32)
+            target_N = torch.tensor(noise.T, dtype=torch.float32)
+            obsp, preds = lam(tensor_O, tensor_Op)
+            act, obs, noi = preds
 
-                    recon_loss = torch.mean(torch.sum(((obsp - tensor_Op) ** 2), axis=1)).item() / do
-                    act_mse = torch.mean(torch.sum(((act - target_A) ** 2), axis=1)).item() / da
-                    obs_mse = torch.mean(torch.sum(((obs - target_O) ** 2), axis=1)).item() / do
-                    if sigma > 0:
-                        noi_mse = torch.mean(torch.sum(((noi - target_N) ** 2), axis=1)).item() / do / (sigma ** 2)
-                    else:
-                        noi_mse = 1.0
+            recon_loss = torch.mean(torch.sum(((obsp - tensor_Op) ** 2), axis=1)).item() / do
+            act_mse = torch.mean(torch.sum(((act - target_A) ** 2), axis=1)).item() / da
+            obs_mse = torch.mean(torch.sum(((obs - target_O) ** 2), axis=1)).item() / do
+            if sigma > 0:
+                noi_mse = torch.mean(torch.sum(((noi - target_N) ** 2), axis=1)).item() / do / (sigma ** 2)
+            else:
+                noi_mse = 1.0
 
-                record.append(dict(
-                    do=do, da=da, dz=dz, db=db, sigma=sigma, iter=i_batch, 
-                    recon_loss=recon_loss, act_mse=act_mse, obs_mse=obs_mse, noi_mse=noi_mse))
+        record.append(dict(
+            do=do, da=da, dz=dz, db=db, sigma=sigma, iter=i_batch, kappa_coeff=kappa_coeff,
+            recon_loss=recon_loss, act_mse=act_mse, obs_mse=obs_mse, noi_mse=noi_mse))
 
-                total_record = pd.DataFrame(record)
-                total_record.to_csv(f'6_learnA{learn_A}_CDzero{CD_zero}_psdaction{pseudo_latent}.csv')
+        total_record = pd.DataFrame(record)
+        total_record.to_csv(f'6_learnA{learn_A}_CDzero{CD_zero}_psdaction{pseudo_latent}.csv')
 
 if __name__ == '__main__':
     main(learn_A=True, CD_zero=False, pseudo_latent=False)
